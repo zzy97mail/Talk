4b6d37868fd5e67c3712354e018a8f4d6b4facba
10156857 <zzy97mail@gail.com>
2020-03-18 17:07:07 +0800

    提交第二个activty

--- app/src/main/java/org/zzy/make/talk/MainActivity.java
+++ app/src/main/java/org/zzy/make/talk/MainActivity.java
@@ -2,13 +2,25 @@ package org.zzy.make.talk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "org.zzy.make.talk.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this,DisplayMessageActivity.class);
+        EditText editText = findViewById(R.id.editText);
+        String msg = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,msg);
+        startActivity(intent);
+    }
+
 }
