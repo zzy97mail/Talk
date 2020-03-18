42ec5f71a6b35d115a74110ae199a352dcca15b1
10156857 <zzy97mail@gail.com>
2020-03-18 18:05:57 +0800

    通知

--- app/src/main/java/org/zzy/make/talk/MainActivity.java
+++ app/src/main/java/org/zzy/make/talk/MainActivity.java
@@ -1,8 +1,13 @@
 package org.zzy.make.talk;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -16,6 +21,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendMessage(View view){
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, MainActivity.EXTRA_MESSAGE)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentTitle("测试")
+                .setContentText("这是一条测试通知")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(1,builder.build());
         Intent intent = new Intent(this,DisplayMessageActivity.class);
         EditText editText = findViewById(R.id.editText);
         String msg = editText.getText().toString();
